c3cc13c03dbe7c77f4585fe46c2d210e84072778
David Fu <david.fu@venly.io>
2024-08-25 16:04:48 +0400

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,32 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div className="main-grid">
+        <nav className="global-nav">
+          
+        </nav>
+        <div className="left-sidebar">
+
+        </div>
+        <div className="main-view">
+
+        </div>
+        <div className="right-sidebar">
+
+        </div>
+        <footer className="now-playing-bar">
+
+        </footer>
+      </div>
+    </>
+  )
+}
+
+export default App
